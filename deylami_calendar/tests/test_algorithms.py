2da9c7dca4312176db886a7d46043cb621149e37
meyt <pasd3000@gmail.com>
2019-08-02 18:43:45 +0430

    Migrate to pytest

--- deylami_calendar/tests/test_algorithms.py
+++ deylami_calendar/tests/test_algorithms.py
@@ -1,51 +1,45 @@
-import unittest
 from deylami_calendar.pure_algorithms import (
     deylami_to_jalali,
     jalali_to_deylami
 )
 
 
-class TestAlgorithms(unittest.TestCase):
+def test_jalali_to_deylami():
+    dy, dm, dd = jalali_to_deylami(1398, 5, 1)
+    assert dy == 1592
+    assert dm == 12
+    assert dd == 15
 
-    def test_jalali_to_deylami(self):
-        dy, dm, dd = jalali_to_deylami(1398, 5, 1)
-        self.assertEqual(dy, 1592)
-        self.assertEqual(dm, 12)
-        self.assertEqual(dd, 15)
-
-        for jy in range(1390, 1395):
-            for jm in range(1, 13):
-                for jd in range(1, 31):
-                    dy, dm, dd = jalali_to_deylami(jy, jm, jd)
-                    assert 1584 <= dy <= 1589
-                    assert 0 <= dm <= 12
-                    assert 0 <= dd <= 30
-
-    def test_deylami_to_jalali(self):
-        jy, jm, jd = deylami_to_jalali(1592, 12, 15)
-        self.assertEqual(jy, 1398)
-        self.assertEqual(jm, 5)
-        self.assertEqual(jd, 1)
-
-        for dy in range(1584, 1589):
-            for dm in range(1, 12):
-                for dd in range(1, 30):
-                    jy, jm, jd = deylami_to_jalali(dy, dm, dd)
-                    assert 1389 <= jy <= 1395
-                    assert 0 <= jm <= 12
-                    assert 0 <= jd <= 31
-
-        # Test panjik days for leap years
-        jy, jm, jd = deylami_to_jalali(1587, 0, 2)
-        self.assertEqual(jy, 1393)
-        self.assertEqual(jm, 1)
-        self.assertEqual(jd, 17)
-
-        jy, jm, jd = deylami_to_jalali(1587, 0, 0)
-        self.assertEqual(jy, 1393)
-        self.assertEqual(jm, 1)
-        self.assertEqual(jd, 15)
+    for jy in range(1390, 1395):
+        for jm in range(1, 13):
+            for jd in range(1, 31):
+                dy, dm, dd = jalali_to_deylami(jy, jm, jd)
+                assert 1584 <= dy <= 1589
+                assert 0 <= dm <= 12
+                assert 0 <= dd <= 30
 
 
-if __name__ == '__main__':  # pragma: no cover
-    unittest.main()
+def test_deylami_to_jalali():
+    jy, jm, jd = deylami_to_jalali(1592, 12, 15)
+    assert jy == 1398
+    assert jm == 5
+    assert jd == 1
+
+    for dy in range(1584, 1589):
+        for dm in range(1, 12):
+            for dd in range(1, 30):
+                jy, jm, jd = deylami_to_jalali(dy, dm, dd)
+                assert 1389 <= jy <= 1395
+                assert 0 <= jm <= 12
+                assert 0 <= jd <= 31
+
+    # Test panjik days for leap years
+    jy, jm, jd = deylami_to_jalali(1587, 0, 2)
+    assert jy == 1393
+    assert jm == 1
+    assert jd == 17
+
+    jy, jm, jd = deylami_to_jalali(1587, 0, 0)
+    assert jy == 1393
+    assert jm == 1
+    assert jd == 15
--- deylami_calendar/tests/test_deylami_date.py
+++ deylami_calendar/tests/test_deylami_date.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from freezegun import freeze_time
 from datetime import datetime
@@ -8,67 +8,62 @@ from deylami_calendar import DeylamiDatetime
 from deylami_calendar.pure_algorithms import deylami_to_jalali
 
 
-class TestDeylamiDate(unittest.TestCase):
+def test_deylami_date():
+    gdate = datetime(2018, 2, 1)
+    ddate = DeylamiDatetime(gdate, tzinfo=TehranTimezone)
 
-    def test_deylami_date(self):
-        gdate = datetime(2018, 2, 1)
-        ddate = DeylamiDatetime(gdate, tzinfo=TehranTimezone)
+    assert ddate.year == 1591
+    assert ddate.month == 6
+    assert ddate.day == 28
 
-        self.assertEqual(ddate.year, 1591)
-        self.assertEqual(ddate.month, 6)
-        self.assertEqual(ddate.day, 28)
+    ddate = DeylamiDatetime(1591, 6, 28, tzinfo=TehranTimezone)
+    assert ddate
 
-        ddate = DeylamiDatetime(1591, 6, 28, tzinfo=TehranTimezone)
-        assert ddate
+    ddate = DeylamiDatetime(1592, 5, 1, tzinfo=TehranTimezone)
+    deylami_date = DeylamiDatetime(ddate)
+    assert deylami_date
 
-        ddate = DeylamiDatetime(1592, 5, 1, tzinfo=TehranTimezone)
-        deylami_date = DeylamiDatetime(ddate)
-        assert deylami_date
+    # Check deylami date return today
+    ddate = DeylamiDatetime().now()
+    jy, jm, jd = deylami_to_jalali(ddate.year, ddate.month, ddate.day)
 
-        # Check deylami date return today
-        ddate = DeylamiDatetime().now()
-        jy, jm, jd = deylami_to_jalali(ddate.year, ddate.month, ddate.day)
+    today = JalaliDatetime.now(TehranTimezone())
+    assert today.year == jy
+    assert today.month == jm
+    assert today.day == jd
 
-        today = JalaliDatetime.now(TehranTimezone())
-        self.assertEqual(today.year, jy)
-        self.assertEqual(today.month, jm)
-        self.assertEqual(today.day, jd)
+    with freeze_time(datetime.now()):
+        deylami_now = DeylamiDatetime(datetime.now()).to_datetime()
+        assert deylami_now.time() == datetime.now().time()
 
-        with freeze_time(datetime.now()):
-            deylami_now = DeylamiDatetime(datetime.now()).to_datetime()
-            self.assertEqual(deylami_now.time(), datetime.now().time())
-
-        now = datetime.now()
-        daylami_date = DeylamiDatetime(now)
-        self.assertEqual(daylami_date.to_date(), now.date())
-
-    def test_limits(self):
-        # Test MinYear and MaxYear
-        with self.assertRaises(ValueError):
-            DeylamiDatetime(194, 1, 1)
-        with self.assertRaises(ValueError):
-            DeylamiDatetime(3373, 1, 1)
-
-        # Test months
-        with self.assertRaises(ValueError):
-            DeylamiDatetime(1592, -1, 3)
-
-        with self.assertRaises(ValueError):
-            DeylamiDatetime(1592, 13, 1)
-
-        # Test days
-        with self.assertRaises(ValueError):
-            DeylamiDatetime(1592, 1, 32)
-        with self.assertRaises(ValueError):
-            DeylamiDatetime(1592, 1, -1)
-
-        # Test days of leap year
-        with self.assertRaises(ValueError):
-            DeylamiDatetime(1595, 0, 0)
-
-        with self.assertRaises(ValueError):
-            DeylamiDatetime(1593, 0, 6)
+    now = datetime.now()
+    daylami_date = DeylamiDatetime(now)
+    assert daylami_date.to_date() == now.date()
 
 
-if __name__ == '__main__':  # pragma: no cover
-    unittest.main()
+def test_limits():
+    # Test MinYear and MaxYear
+    with pytest.raises(ValueError):
+        DeylamiDatetime(194, 1, 1)
+    with pytest.raises(ValueError):
+        DeylamiDatetime(3373, 1, 1)
+
+    # Test months
+    with pytest.raises(ValueError):
+        DeylamiDatetime(1592, -1, 3)
+
+    with pytest.raises(ValueError):
+        DeylamiDatetime(1592, 13, 1)
+
+    # Test days
+    with pytest.raises(ValueError):
+        DeylamiDatetime(1592, 1, 32)
+    with pytest.raises(ValueError):
+        DeylamiDatetime(1592, 1, -1)
+
+    # Test days of leap year
+    with pytest.raises(ValueError):
+        DeylamiDatetime(1595, 0, 0)
+
+    with pytest.raises(ValueError):
+        DeylamiDatetime(1593, 0, 6)
